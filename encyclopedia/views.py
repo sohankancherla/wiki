24a9c280d3a85a6bfb5ce80365dd9ad52f6f7c74
skancher <sohank@gmail.com>
2024-01-24 20:26:00 -0800

    Add view and route for entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
+from markdown import markdown
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, name):
+    content = util.get_entry(name)
+    return render(request, "encyclopedia/entry.html", {
+        "title": name,
+        "content": markdown(content)
+
     })
 
